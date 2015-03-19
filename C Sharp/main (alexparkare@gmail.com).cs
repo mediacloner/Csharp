037da3d6751c2e9063b829dfeb182b2ddb944f73
Alex <alexbcn@gmail.com>
2015-03-19 07:17:36 +0100

    1.1.1

---
+++ C Sharp/main (alexparkare@gmail.com).cs
@@ -0,0 +1,42 @@
+﻿/* Alex Sánchez 18/03/2014   */
+/*...........................*/
+
+//(8.4.1) Un programa que pida al usuario que teclee frases, y las almacene en el
+//fichero "registro.txt", que puede existir anteriormente (y que no deberá borrarse, sino
+//añadir al final de su contenido). Cada sesión acabará cuando el usuario pulse Intro sin
+//teclear nada.
+
+using System; 
+using System.IO; // Para StreamReader
+
+public class WriteExistFileTxt
+{
+   
+    public static void Main()
+    {   
+        string frase="vacio";
+        StreamWriter fichero;
+        Console.WriteLine("Por favor, introduzca la información que desea insertar:");
+        while (frase = null)
+        {
+            frase = Convert.ToString(Console.ReadLine());
+            fichero = File.CreateText("prueba2.txt");
+        }
+        Console.WriteLine("Por favor, introduzca la segunda tanda de datos que desea insertar:");
+        while (frase = null)
+        {
+            frase = Convert.ToString(Console.ReadLine());
+            fichero = File.CreateText("prueba2.txt");
+        } Console.WriteLine("Por favor, introduzca la tercera tanda de datos que desea insertar:");
+        while (frase = null)
+        {
+            frase = Convert.ToString(Console.ReadLine());
+            fichero = File.CreateText("prueba2.txt");
+        }
+        fichero.WriteLine("Primera línea"); 
+        fichero.Close();
+        fichero = File.AppendText("prueba2.txt"); 
+        fichero.WriteLine("Segunda línea"); 
+        fichero.Close();
+}
+}
---
+++ C Sharp/main.cs
@@ -0,0 +1,24 @@
+﻿/* Alex Sánchez 17/03/2014   */
+/*...........................*/
+
+//(8.2.1) Crea un programa que lea las tres primeras líneas del fichero 
+//creado en el apartado anterior y las muestre en pantalla.
+
+using System; 
+using System.IO; // Para StreamReader
+
+public class ReadFileTxt
+{
+    public static void Main()
+    {
+        StreamReader fichero;
+        string linea;
+        fichero = File.OpenText("registroDeUsuario.txt");
+        linea = fichero.ReadLine();
+        Console.WriteLine(linea);
+        Console.WriteLine(fichero.ReadLine());
+        Console.WriteLine(fichero.ReadLine());
+        fichero.Close();
+        Console.ReadLine();
+    }
+}
